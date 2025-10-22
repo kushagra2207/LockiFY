bb5132ef26640ae83e532cf2909e20525e18d68f
kushagra2207 <fantastick22072006@gmail.com>
2025-10-22 06:08:20 +0530

    feat: add password modification routes

---
+++ backend/routes/passwordRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const { verifyToken } = require('../middleware/auth')
+const { 
+    getPasswords, 
+    addPassword, 
+    updatePassword, 
+    deletePassword 
+} = require('../controllers/passwordController')
+
+const router = express.Router()
+
+router.use(verifyToken)
+
+router.get('/', getPasswords)
+router.post('/', addPassword)
+router.put('/:id', updatePassword)
+router.delete(':id', deletePassword)
+
+module.exports = router
