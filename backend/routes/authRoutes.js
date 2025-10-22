b1cd4f15ef4afa945a63c3cdd99cf9ef87a861f5
kushagra2207 <fantastick22072006@gmail.com>
2025-10-23 04:46:30 +0530

    fix: redirect url

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,7 +5,7 @@ const { googleAuthSuccess, logout, me } = require('../controllers/authController
 const router = express.Router()
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), googleAuthSuccess)
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: `${process.env.FRONTEND_URL}/` }), googleAuthSuccess)
 
 router.get('/me', me)
 router.post('/logout', logout)
