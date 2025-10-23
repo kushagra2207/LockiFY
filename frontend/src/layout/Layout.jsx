d38a7d812fa28a7c292cbd3cf0aad6b9b31a12b9
kushagra2207 <fantastick22072006@gmail.com>
2025-10-23 19:32:49 +0530

    style: add background

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
     return (
         <div>
             <Navbar />
-            <main>
+            <main className="bg-blue-100 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
                 <Outlet />
             </main>
             <ToastContainer
