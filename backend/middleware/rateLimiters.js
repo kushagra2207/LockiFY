b514eeab4878e2d8f6a409ee7ae63724d8e006cb
kushagra2207 <fantastick22072006@gmail.com>
2025-12-02 15:03:57 +0530

    update rate limit

--- backend/middleware/rateLimiters.js
+++ backend/middleware/rateLimiters.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit')
 
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 20,
+    max: 40,
     message: "Too many authentication attempts, please try again later",
     standardHeaders: true,
     legacyHeaders: false,
